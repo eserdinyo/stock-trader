a3cd94fc18f90ddfc13ab43a991caec47ed4122f
Muhammet ESER <muhammeteser29@gmail.com>
2018-09-30 23:44:20 +0300

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+ <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+ <router-link class="navbar-brand" to="/">Stock Trader</router-link>
+ 
+
+  <div class="collapse navbar-collapse">
+    <ul class="navbar-nav mr-auto">
+      <router-link to="/portfolio" class="nav-link" active-class="active"><a>Portfolio</a></router-link>
+      <router-link to="/stocks" class="nav-link"  active-class="active"><a>Stocks</a></router-link>
+    </ul>
+
+    <ul class="navbar-nav ml-auto mr-auto">
+      <a href="#" class="nav-link">End Day</a>
+    </ul>
+    <ul class="navbar-nav ml-auto">
+      <a href="#" class="nav-link"><a>Save</a></a>
+      <a href="#" class="nav-link"><a>Load</a></a>
+    </ul>
+    
+  </div>
+</nav>
+</template>
